78f111a1294ee76b2302974fa9fc92269f6e58e3
saran.su <suwannachatkul.s@gmail.com>
2023-09-23 10:07:49 +0900

    Update config for pydantic 2

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,16 @@
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DB_USER: str = Field(env='IMAGEFASTAPI_DB_USER')
-    DB_PASS: str = Field(env='IMAGEFASTAPI_DB_PASSWORD')
+    model_config = SettingsConfigDict(env_prefix='IMAGEFASTAPI_')
+    DB_USER: str
+    DB_PASSWORD: str
     DB_HOST: str = "localhost"
     DB_PORT: str = "5432"
     DB_NAME: str = "myimage_fastapi"
 
     @property
     def DATABASE_URL(self):
-        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 settings = Settings()
