aada286a88d251fe91ee50d9a1b78c01afc81bd1
saran.su <suwannachatkul.s@gmail.com>
2023-09-27 13:43:31 +0900

    Add schemas for patch and create_date validation

--- app/schemas/image_info.py
+++ app/schemas/image_info.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel, model_validator, validator
-from datetime import datetime
 import json
+from datetime import datetime
 from typing import List, Optional
+
+from pydantic import BaseModel, field_validator, model_validator
 
 from .tag import TagBase
 
@@ -13,18 +14,24 @@ class ImageInfoBase(BaseModel):
 
 
 class ImageInfoCreateQuery(BaseModel):
-    ext: Optional[str] = 'jpg'
+    ext: Optional[str] = "jpg"
 
 
 class ImageInfoCreate(ImageInfoBase):
     tags: List[str] = []
 
-    @model_validator(mode='before')
+    @model_validator(mode="before")
     @classmethod
     def validate_to_json(cls, value):
         if isinstance(value, str):
             return cls(**json.loads(value))
         return value
+
+
+class ImageInfoUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    tags: Optional[List[str]] = None
 
 
 class ImageInfo(ImageInfoBase):
@@ -47,7 +54,8 @@ class ImageInfoFilters(BaseModel):
     created_date__before: Optional[str] = None
     random: Optional[bool] = False
 
-    @validator("created_date", "created_date__after", "created_date__before", pre=True, always=True)
+    @field_validator("created_date", "created_date__after", "created_date__before", mode="before")
+    @classmethod
     def validate_date_format(cls, date_str: str):
         if date_str:
             try:
@@ -56,3 +64,14 @@ class ImageInfoFilters(BaseModel):
             except ValueError:
                 raise ValueError("Invalid date format. Expected YYYYMMDD.")
         return date_str
+
+    @model_validator(mode="after")
+    def validate_date_conflict(self):
+        created_date = self.created_date
+        created_date__after = self.created_date__after
+        created_date__before = self.created_date__before
+
+        if created_date and (created_date__after or created_date__before):
+            raise ValueError("created_date cannot be used with created_date__after or created_date__before.")
+
+        return self
