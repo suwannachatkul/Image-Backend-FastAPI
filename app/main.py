1fcbffcc65be0bf520c28eba3dc4745325f44079
saran.su <suwannachatkul.s@gmail.com>
2023-09-27 13:43:54 +0900

    Add exception handler for validation error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import ValidationError
+
 from app.api.router import router as api_router
 
 app = FastAPI()
+
+@app.exception_handler(ValidationError)
+async def validation_exception_handler(request: Request, exc: ValueError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder(
+            {"detail": exc.errors()}
+        ),
+    )
+
 
 app.include_router(api_router)
